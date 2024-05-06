9ec139aeb696d3ff88a0a99e567a3c8c22d0f517
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2024-05-06 16:37:01 +0200

    fix: Logger couldnt initialize

--- back/initializers/logger.go
+++ back/initializers/logger.go
@@ -2,18 +2,13 @@ package initializers
 
 import (
 	"go.uber.org/zap"
-	"log"
 )
 
 var Logger *zap.SugaredLogger
 
 func InitLogger() {
 	zapLogger := zap.NewExample()
-	defer func(zapLogger *zap.Logger) {
-		err := zapLogger.Sync()
-		if err != nil {
-			log.Println("Error while initializing logger")
-		}
-	}(zapLogger)
+	defer zapLogger.Sync()
 	Logger = zapLogger.Sugar()
+	Logger.Infoln("Logger initialized")
 }
